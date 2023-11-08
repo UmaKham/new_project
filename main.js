549eba155b2f440dd1f14b50745a2e0e6592e79a
Ulmas Khamrokulov <uma.kham94@gmail.com>
2023-11-09 04:13:25 +0500

    fixed

--- main.js
+++ main.js
@@ -1,20 +1,101 @@
-const ctx = document.getElementById('myChart');
+const chartData = {
+  labels: ["BitCoin", "Ethereum", "Dash"],
+  data: [64, 18, 18]
+}
 
-  new Chart(ctx, {
-    type: 'Daughnut',
+const doughnutChart = document.getElementById('myChart');
+
+  new Chart(doughnutChart, {
+    type: 'doughnut',
     data: {
-      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-      datasets: [{
-        label: '# of Votes',
-        data: [12, 19, 3, 5, 2, 3],
-        borderWidth: 1
-      }]
+      labels: chartData.labels,
+      datasets: [
+        {
+          label: "Language Popularity",
+          data: chartData.data
+        }
+      ] 
     },
     options: {
-      scales: {
-        y: {
-          beginAtZero: true
+      borderWidth: false,
+      hoverBorderWidth: 2,
+      backgroundColor: ["#010155", "#642999", "#912491"],
+      rotation: 200,
+      cutout: 55,
+      plugins: {
+        legend: {
+          display: false,
         }
       }
     }
   });
+
+  const lineChart = document.getElementById('lineChart');
+  new Chart(lineChart, {
+    type: "line",
+    data: {
+      labels: chartData.labels,
+      datasets: [
+        {
+          label: "Language Popularity",
+          data: chartData.data
+        }
+      ] 
+    },
+    options: {
+      borderWidth: false,
+      hoverBorderWidth: 2,
+      borderWidth: 1,
+      borderColor: "grey",
+      pointBorderColor: "white",
+      pointRadius: 5,
+      backgroundColor: ["#010155", "#642999", "#912491"],
+      rotation: 200,
+      plugins: {
+        legend: {
+          display: false,
+        }
+      }
+    }
+  });
+  
+  const transactionChart = document.getElementById('transactionChart');
+  new Chart(transactionChart, {
+    type: "line",
+    data: {
+      labels: chartData.labels,
+      datasets: [
+        {
+          label: "Language Popularity",
+          data: chartData.data
+        }
+      ] 
+    },
+    options: {
+      borderWidth: false,
+      hoverBorderWidth: 2,
+      borderWidth: 1,
+      borderColor: "grey",
+      pointBorderColor: "white",
+      pointRadius: 5,
+      backgroundColor: ["#010155", "#642999", "#912491"],
+      rotation: 200,
+      plugins: {
+        legend: {
+          display: false,
+        }
+      }
+    }
+  });
+
+  let ul = document.querySelector('.card ul')
+
+  function balancePercent() {
+    chartData.labels.forEach((el, i) => {
+      let li = document.createElement('li');
+      li.innerHTML = `${el}: <span>${chartData.data[i]}%</span>`;
+      ul.appendChild(li)
+    })
+  }
+
+  balancePercent();
